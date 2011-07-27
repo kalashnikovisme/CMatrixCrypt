8c3e2f94517968a2d5139e13b81ca47aee810941
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-07-28 00:01:47 +0200

    made simple tests for both encryption and decyption

--- src/ascii86_tests.cpp
+++ src/ascii86_tests.cpp
@@ -12,19 +12,32 @@ int main(void) {
 
   // test for hello, world
   enc << "Hello, World";
+  enc.close();
   enc >> tmp;
   assert(tmp == "<~87cURD_*#4DfTZ)~>");
   enc.reset(); tmp.clear();
   
   // test for somethin' longer
   enc << "The advantage";
+  enc.close();
   enc >> tmp;
   assert(tmp == "<~<+ohc@:XOiDKK<\"AH~>");
   enc.reset(); tmp.clear();
 
   // test for one more sentence
   enc << "This should ensure that the algorithm is working allright. ";
+  enc.close();
   enc >> tmp;
   assert(tmp == "<~<+oue+EM47F_kS2ASuR3Eb-A8BOQ!*FD,5.@;KRpEbTW/D'3P6+EqaECM@[!+CT)&EbT0\"F=n[~>");
   enc.reset(); tmp.clear();
+  
+  // let's try this, shall we?
+  cme::Decode86 dec;
+  dec.debug();
+  dec << "<~<+oue+EM47F_kS2ASuR3Eb-A8BOQ!*FD,5.@;KRpEbTW/D'3P6+EqaECM@[!+CT)&EbT0\"F=n[~>";
+  dec.debug();
+  dec >> tmp;
+  dec.debug();
+  assert(tmp == "This should ensure that the algorithm is working allright. ");
+  dec.reset(); tmp.clear();
 }
