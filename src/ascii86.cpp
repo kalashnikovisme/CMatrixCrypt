9fa22f7d1398a96febf05a82767167d39b5415f0
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-07-27 15:17:25 +0200

    turned the ascii86 encode function into a class, now preparing to do the same with the decode function

--- src/ascii86.cpp
+++ src/ascii86.cpp
@@ -1,5 +1,5 @@
 #include "ascii86.hpp"
-
+/*
 // IMPLEMENTATIONS
 // encode a tuple, which is a string of 4 characters
 string encode86_tuple(string tuple) {
@@ -81,4 +81,153 @@ string decode86(string data) {
   dec.erase(dec.size()-padded_chars, padded_chars); // erase the superflous characters at the end
 
   return(dec);                                      // return the encoded string
+}
+*/
+
+namespace cme {
+  /* CONSTRUCTORS */
+  Encode86::Encode86() {
+    reset();
+  }
+
+  Encode86::Encode86(std::string data) {
+    reset();
+    write(data);
+  }
+
+  /* ENCODING FACILITIES */
+  void Encode86::encode_tuple(std::string tuple) {
+    std::string enc = "";      // encoded string
+    long int int_rep = 0; // integer representation of the string, this is a LONG int so that it's min. 32bit
+
+    long int multiplier = 256*256*256;                // turn the tuple into an int, this is, too, a LONG int
+    for(int i = 0; i < tuple.length(); i++) {         // loop thru the characters
+      int_rep += multiplier * (unsigned char)tuple[i];// add the byte to the int_representation. coerce this to unsigned char because 
+      multiplier /= 256;                              // increase the multiplier
+    }
+
+    unsigned char chr;           // store current character
+    for(int i = 0; i < 5; i++) { // loop 5 times
+      chr = (int_rep%85)+33;     // extract a character
+      enc.insert(0, 1, chr);     // insert the character at the beginning (the extraction process is in reverse)
+      int_rep /= 85;             // prime the int for the next extraction
+    }
+
+    outbuf += enc;  // add the encoded stuff to the outbuffer
+  }
+
+  void Encode86::encode() {
+    if(!closed_input) {
+      int amount = inbuf.size()/4;
+      int pos;
+      for(pos = 0; pos < amount; ++pos) {
+        encode_tuple(inbuf.substr(pos*4, 4));
+      }
+      inbuf.erase(0, amount*4);
+    }
+  }
+
+  /* OUTPUT FACILITIES */
+  // get a single character
+  char Encode86::get() const {
+    if(outpos < outbuf.size()) {
+      ++outpos;
+      return(outbuf[outpos]);
+    } else {
+      return(0);
+    }
+  }
+
+  // read as much data as there is
+  std::string Encode86::read() const {
+    if((outbuf.size() - outpos) > 0) {
+      return(outbuf.substr(outpos, (outbuf.size() - outpos)));
+    } else {
+      return("");
+    }
+  }
+
+  std::string Encode86::data() const {
+    return(outbuf);
+  }
+
+  /* INPUT FACILITIES */
+  // push a single character
+  void Encode86::put(char chr) {
+    if(!closed_input) {
+      inbuf.push_back(chr);
+      encode(); // tell the class to try to encode the data we got
+    }
+  }
+
+  void Encode86::write(std::string data) {
+    if(!closed_input) {
+      inbuf += data;
+      encode();
+    }
+  }
+
+  /* OPERATOR OVERLOADS */
+  // write to a string
+  Encode86& Encode86::operator>>(std::string& out) {
+    close();
+    out += read();
+    return(*this);
+  }
+
+  Encode86& Encode86::operator<<(const std::string& in) {
+    if(!closed_input) {
+      write(in);
+      encode();
+    }
+    return(*this);
+  }
+
+  /* OTHER STUFF */
+  // check if the input is closed
+  bool Encode86::closed() const {
+    return(closed_input);
+  }
+
+  // close the input
+  void Encode86::close() {
+    if(!closed_input) {
+      encode();             // encode the rest of the stuff in the input buffer
+      closed_input = true;  // prevent new input from coming in
+      
+      int appendixes = (inbuf.size() % 4);  // find out how many characters are missing
+      if(appendixes != 0) {
+        for(int i = 0; i < (4-appendixes); ++i) { 
+          inbuf.push_back('\0');              // add the null characters
+        }
+
+        // encode the last four characters
+        encode_tuple(inbuf.substr(inbuf.size()-4, 4));
+
+        for(int i = 0; i < (4-appendixes); ++i) { // remove the unneeded characters
+          outbuf.erase(outbuf.size()-1, 1);
+        }
+      }
+
+      // reset inbuf
+      inbuf = "";
+
+      // finish outbuf
+      outbuf += "~>";
+    }
+  }
+
+  void Encode86::reset() {
+    closed_input = false;
+    inbuf = "";
+    outbuf = "<~";
+    outpos = 0;
+  }
+
+  void Encode86::debug() {
+    std::cout << "inbuf: " << inbuf << std::endl;
+    std::cout << "outbuf: " << outbuf << std::endl;
+    std::cout << "outpos: " << outpos << std::endl;
+    std::cout << "closed: " << closed_input << std::endl;
+  }
 }
--- src/ascii86.hpp
+++ src/ascii86.hpp
@@ -1,13 +1,50 @@
 #pragma once
-
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
 #include <string>
-using namespace std;
+#include <sstream>
 
 // PROTOTYPES
-string encode86(string data); //encodes a string of any length
-string decode86(string code); //decodes a string of any length
-string encode86_tuple(string tuple); //encodes a tuple (of 4 characters)
-string decode86_tuple(string tuple); //decodes a tuple (of 5 characters)
+//std::string encode86(std::string data); //encodes a string of any length
+//std::string decode86(std::string code); //decodes a string of any length
+//std::string encode86_tuple(std::string tuple); //encodes a tuple (of 4 characters)
+//std::string decode86_tuple(std::string tuple); //decodes a tuple (of 5 characters)
+
+namespace cme {
+  class Encode86 {
+    private:
+      // buffers for input/output
+      std::string inbuf, outbuf;
+      // position for outputting
+      mutable int outpos;
+      // whether the input is closed
+      bool closed_input;
+
+      void encode_tuple(std::string);
+      void encode();
+    public:
+      // constructors
+      Encode86();           // empty constructur
+      Encode86(std::string);// fill up the input buffer with std::string
+
+      // read encoded data
+      char get() const;             // reads just one character
+      std::string read() const;     // reads as much data as there is in the output buffer
+      std::string data() const;     // reads the whole output buffer
+
+      // write data to be encoded
+      void put(char);           // write a single character
+      void write(std::string);  // write some data
+
+      // operator overloads
+      Encode86& operator>>(std::string&);         // write output to string
+      Encode86& operator<<(const std::string&);   // read from string
+
+      // informative
+      bool closed() const;// check if the input is closed (this needs to be done explicitly)
+      void close();       // close the input
+      void reset();       // reset everything
+      void debug();
+  };
+}
