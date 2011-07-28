37a515bebd3f3c555baf72a80ec065a9447ab9f0
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-07-28 18:46:24 +0200

    now I found out how I can do deflation with zlib... wrote the header already, now on to implementing it

--- src/gzip.hpp
+++ src/gzip.hpp
@@ -9,3 +9,42 @@
 
 std::string compress_string(const std::string& str, int compressionlevel /*= Z_BEST_COMPRESSION*/);
 std::string decompress_string(const std::string& str);
+
+namespace cme {
+  class Deflate {
+    private:
+      char **output_buffer;   // buffer to read zlib's output from
+      int output_buffer_size; // keep track of how big the buffer is
+      std::string output;     // where to store the output
+
+      z_stream compress_stream; // zlib compression stream
+      bool closed;              // whether or not this is closed
+      mutable int outpos;       // where we are in the outputting
+    public:
+      Deflate();                // default
+      Deflate(int);             // specific bufsize
+      Deflate(std::string);     // string to start deflating
+      Deflate(int, std::string);// specific bufsize and string to start deflating
+      ~Deflate();               // destructor
+
+      // OUTPUT FUNCTIONS
+      char get() const;         // read a single character
+      std::string read() const; // read new data
+      std::string data() const; // read all data
+
+      // INPUT FUNCTIONS
+      void put(char);           // write a single character
+      void write(std::string);  // write a string
+
+      // OPERATORS
+      Deflate& operator<<(const std::string&);
+      Deflate& operator>>(std::string&) const;
+
+      // MISC
+      bool closed();
+      void close();
+      void setup();
+      void reset();
+      bool eol();
+  }
+}
--- src/gzip_tests.cpp
+++ src/gzip_tests.cpp
@@ -1,5 +1,89 @@
 #include "gzip.hpp"
 using namespace std;
 
+// try compressing multiple string with gzip
 int main(void) {
+  string tst1, tst2;
+  tst1 = "Harrow, Harrow, said the Korean, ";
+  tst2 = "who likes to eat pie. ";
+  int compressionlevel = Z_BEST_COMPRESSION;
+
+  // MEMORY STRUCTURES
+  z_stream zs;                        // z_stream is zlib's control structure
+  std::string outstring;
+  int ret;
+  char outbuffer[32768];
+  memset(&zs, 0, sizeof(zs));
+ 
+  // INITIALIZE
+  if (deflateInit(&zs, compressionlevel) != Z_OK)
+    throw(std::runtime_error("deflateInit failed while compressing."));
+
+  // SET UP INPUT
+  zs.next_in = (Bytef*)tst1.data();
+  zs.avail_in = tst1.size();           // set the z_stream's input
+
+  // COMPRESSION
+
+  // retrieve the compressed bytes blockwise
+  do {
+    zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+    zs.avail_out = sizeof(outbuffer);
+
+    ret = deflate(&zs, Z_NO_FLUSH);
+
+    if (outstring.size() < zs.total_out) {
+      // append the block to the output string
+      outstring.append(outbuffer,
+          zs.total_out - outstring.size());
+    }
+  } while (ret == Z_OK);
+  // END OF COMPRESSION
+
+  // SET UP INPUT2
+  zs.next_in = (Bytef*)tst2.data();
+  zs.avail_in = tst2.size();           // set the z_stream's input
+
+  // COMPRESSION
+
+  // retrieve the compressed bytes blockwise
+  do {
+    zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+    zs.avail_out = sizeof(outbuffer);
+
+    ret = deflate(&zs, Z_NO_FLUSH);
+
+    if (outstring.size() < zs.total_out) {
+      // append the block to the output string
+      outstring.append(outbuffer,
+          zs.total_out - outstring.size());
+    }
+  } while (ret == Z_OK);
+  // END OF COMPRESSION
+  zs.next_in = NULL;
+  zs.avail_in = 0;           // set the z_stream's input
+
+  do {
+    zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+    zs.avail_out = sizeof(outbuffer);
+
+    ret = deflate(&zs, Z_FINISH);
+
+    if (outstring.size() < zs.total_out) {
+      // append the block to the output string
+      outstring.append(outbuffer,
+          zs.total_out - outstring.size());
+    }
+  } while (ret == Z_OK);
+  
+  // FINALIZE
+  deflateEnd(&zs);
+
+  if (ret != Z_STREAM_END) {          // an error occurred that was not EOF
+    std::ostringstream oss;
+    oss << "Exception during zlib compression: (" << ret << ") " << zs.msg;
+    throw(std::runtime_error(oss.str()));
+  }
+
+  cout << decompress_string(outstring) << endl;
 }
