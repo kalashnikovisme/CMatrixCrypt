80bd3cd3b6a62327baf5153d93ecdb5b5f51d761
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-08-01 23:33:44 +0200

    added the option to read from stdin to deflate86

--- src/deflate86.cpp
+++ src/deflate86.cpp
@@ -5,6 +5,9 @@
 #include "ascii86.hpp"
 using namespace std;
 
+void read_file(cme::Deflate&, cme::Encode86&, string filename);
+void read_input(cme::Deflate&, cme::Encode86&);
+
 int main(int argc, char *argv[]) {
   if(argc < 2) {
     cout << "please specify a file to compress as argument" << endl;
@@ -13,12 +16,28 @@ int main(int argc, char *argv[]) {
 
   cme::Deflate deflate;
   cme::Encode86 encode;
-  ifstream input(argv[1]);
+  string file(argv[1]);
+
+  if(file == "-") {
+    read_input(deflate, encode);
+  } else {
+    read_file(deflate, encode, file);
+  }
+
+  deflate.close();
+  encode << deflate.read();
+  encode.close();
+  cout << encode.read();
+  cout << endl;
+}
+
+void read_file(cme::Deflate& deflate, cme::Encode86& encode, string filename) {
+  ifstream input(filename.data());
   string line;
 
   if(!input.good()) {
-    cout << "error opening file" << endl;
-    return(-1);
+    cout << "error opening file " << filename << endl;
+    exit(-1);
   }
 
   while(!input.eof()) {
@@ -27,10 +46,20 @@ int main(int argc, char *argv[]) {
     encode << deflate.read();
     cout << encode.read();
   }
+}
 
-  deflate.close();
-  encode << deflate.read();
-  encode.close();
-  cout << encode.read();
-  cout << endl;
+void read_input(cme::Deflate& deflate, cme::Encode86& encode) {
+  string line;
+
+  if(!cin.good()) {
+    cout << "error opening stdin" << endl;
+    exit(-1);
+  }
+
+  while(!cin.eof()) {
+    getline(cin, line);
+    deflate << line;
+    encode << deflate.read();
+    cout << encode.read();
+  }
 }
