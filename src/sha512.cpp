f9e220813e9614b8b24a720f91049d8cf4604e1b
Patrick M. Elsen <pelsen.vn@gmail.com>
2011-09-06 10:54:11 +0200

    working on the SHA512 class, not done yet, need to finish up and add tests.

--- src/sha512.cpp
+++ src/sha512.cpp
@@ -1,5 +1,125 @@
 #include "sha512.hpp"
 
 namespace cme {
+	void SHA512::sha4_starts(int is384) {
+		if(closed_input) {
+			throw std::runtime_error("sha4 input already closed");
+		} else {
+			polarssl::sha4_starts(&sha4_ctx, (is384) ? 1 : 0);
+		}
+	}
+
+	void SHA512::sha4_update(const unsigned char* input, size_t ilen) {
+		if(closed_input) {
+			throw std::runtime_error("sha4 input already closed");
+		} else {
+			sha4_update(&sha4_ctx, input, ilen);
+		}
+	}
+
+	void SHA512::sha4_finish() {
+		if(closed_input) {
+			throw std::runtime_error("sha4 input already closed");
+		} else {
+			sha4_finish(&sha4_ctx, hash);
+		}
+	}
+
+	void SHA512::close() {
+		sha4_finish();
+		closed_input = true;
+	}
+
+	bool SHA512::closed() {
+		return(closed_input);
+	}
+
+	SHA512::SHA512() {
+		closed_input = false;
+		sha4_starts(0);
+	}
+
+	SHA512::SHA512(std::string data) {
+		closed_input = false;
+		sha4_starts(0);
+    write(data);
+	}
+
+	void SHA512::write(std::string& input) {
+		sha4_update( (const unsigned char *) input.c_str(), input.size() );
+	}
+
+	void SHA512::hash(unsigned char data[64]) {
+		// check if the input is closed (and thus the hash is available)
+		if(!closed_input) {
+			throw std::runtime_error("sha4 input not closed yet");
+		} else {
+			// copy the hash into the array
+			for(int i = 0; i < 64; ++i) {
+				data[i] = hash[i];
+			}
+		}
+	}
+
+	std::string SHA512::hash() {
+		// check if the input is closed (and thus the hash is available)
+		if(!closed_input) {
+			throw std::runtime_error("sha4 input not closed yet");
+		} else {
+			// package the hash as a string and return
+			return(string( (char *) hash, 64));
+		}
+	}
+
+	std::string SHA512::hexdigest() {
+		// check if the input is closed (and thus the hash is available)
+		if(!closed_input) {
+			throw std::runtime_error("sha4 input not closed yet");
+		} else {
+			// declare a stringstream to hold the hex data
+			std::stringstream hexdigest;
+			// tell it that we want hex stuff
+			hexdigest << std::hex;
+
+			// add all digest digits
+			for(int i = 0; i < 64; ++i) {
+				hexdigest << hash[i];
+			}
+
+			// return the resulting string
+			return(hexdigest.str());
+		}
+	}
+
+	bool compare(const std::string ohash) {
+		if(!closed_input) {
+			throw std::runtime_error("sha4 input not closed yet");
+		}
+
+		switch(ohash.size()) {
+			case 64:
+				return(this->hash() == ohash);
+				break;
+			case 128:
+				return(this->hexdigest() == ohash);
+				break;
+			default:
+				throw std::runtime_error("invalid hash provided for comparison");
+		}
+	}
+
+	bool compare(const unsigned char data[64]) {
+		if(!closed_input) {
+			throw std::runtime_error("sha4 input not closed yet");
+		}
+
+		for(int i = 0; i < 64; ++i) {
+			if(hash[i] != data[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 
 }
--- src/sha512.hpp
+++ src/sha512.hpp
@@ -1,8 +1,66 @@
 #include "polarssl/sha4.h"
 #include <string>
+#include <stringstream>
 #include <iostream>
+#include <stdexcept>
 #pragma once
 
 namespace cme {
+	class SHA512 {
+		private:
+			// used by polarssl for hashing
+			polarssl::sha4_context sha4_ctx;
+			// store the final hash (kinda like caching)
+			unsigned char hash[64];
+			// is it closed?
+			bool closed_input;
+			
+			// sha4 wrapper functions (these access polarssl)
+			void sha4_starts(int is384);
+			void sha4_update(const unsigned char* input, size_t ilen);
+			void sha4_finish();
+		public:
+			// default constructor, initializes sha4 hashing
+			SHA512();
+			// constructor with string argument, initializes and starts hashing the string
+			SHA512(std::string);
 
+			// add a string to the hash
+			void write(std::string&);
+
+			// close the input (finishes the hash)
+			void close();
+
+			// check if it's closed
+			bool closed();
+
+			// get the hash as raw data
+			void hash(unsigned char data[64]) const;
+
+			// get the hash as string
+			std::string hash() const;
+
+			// or get it as a hex encoded string
+			std::string hexdigest() const;
+
+			// compare this hash to another hash
+			bool compare(const std::string) const;
+			bool compare(const unsigned char data[64]);
+
+			// interaction with cout and cin
+			std::ostream& operator<<(std::ostream&, const SHA512&);
+			std::istream& operator>>(std::istream&, SHA512&);
+
+			// string input and output
+			SHA512& operator<<(const std::string&);
+			SHA512& operator>>(std::string&);
+
+			// comparing hashes
+			bool operator==(const SHA512&);
+			bool operator==(const std::string&);
+			bool operator==(const unsigned char data[64]);
+			bool operator!=(const SHA512&);
+			bool operator!=(const std::string&);
+			bool operator!=(const unsigned char data[64]);
+	}
 }
